6667b7af9782b479c8eacb40434b8ad4c1b1bb32
BaronKhan <bksonic@hotmail.co.uk>
2018-12-30 20:52:09 +0000

    Rename files

--- emscripten_hacks/build.sh
+++ emscripten_hacks/build.sh
@@ -2,4 +2,4 @@ emcmake cmake -DCMAKE_C_FLAGS="-Oz" -DAPPLY_EMSCRIPTEN_HACKS=ON -DSONAME=OFF -DU
 emcmake cmake -build .
 emmake make
 echo "building libgit2.js"
-emcc -s ALLOW_MEMORY_GROWTH=1 -s ASSERTIONS=1 -s FORCE_FILESYSTEM=1 --post-js jsinit.js -s "EXTRA_EXPORTED_RUNTIME_METHODS=['FS']" -Oz jslib.c libgit2.a -Isrc -I../src -I../include -o libgit2.js
+emcc -s ALLOW_MEMORY_GROWTH=1 -s ASSERTIONS=1 -s FORCE_FILESYSTEM=1 --post-js libgit2init.js -s "EXTRA_EXPORTED_RUNTIME_METHODS=['FS']" -Oz jslibgit2.c libgit2.a -Isrc -I../src -I../include -o libgit2.js
